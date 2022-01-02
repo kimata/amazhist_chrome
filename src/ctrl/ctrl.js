e365eb998004b986884b954dca5a557dfc66e6c4
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-01-03 01:27:48 +0900

    feat: 合計金額と残り時間の表示に対応

--- src/ctrl/ctrl.js
+++ src/ctrl/ctrl.js
@@ -1,18 +1,22 @@
+var start_time = null
 var year_list = []
 var year_done = 0
 var item_list = []
 var order_info = {
-    total: 0,
-    done: 0
+    total_count: 0,
+    done_count: 0,
+    total_price: 0
 }
 
 function init_status() {
+    start_time = new Date()
     year_list = []
     year_done = 0
     item_list = []
     order_info = {
-        total: 0,
-        done: 0
+        total_count: 0,
+        done_count: 0,
+        total_price: 0
     }
 
     document.getElementById('log').value = ''
@@ -20,14 +24,32 @@ function init_status() {
 }
 
 function notify_progress() {
-    document.getElementById('order_count_done').innerText = order_info['done'].toLocaleString()
-    document.getElementById('order_count_total').innerText = order_info['total'].toLocaleString()
+    document.getElementById('order_count_done').innerText = order_info['done_count'].toLocaleString()
+    document.getElementById('order_count_total').innerText = order_info['total_count'].toLocaleString()
+    document.getElementById('order_price_total').innerText = order_info['total_price'].toLocaleString()
 
-    rate = Math.round((100 * order_info['done']) / order_info['total']) + '%'
+    rate = Math.round((100 * order_info['done_count']) / order_info['total_count'])
 
     progress_bar = document.getElementById('progress_bar')
-    progress_bar.innerText = rate
-    progress_bar.style.width = rate
+    progress_bar.innerText = rate + '%'
+    progress_bar.style.width = rate + '%'
+
+    if (order_info['done_count'] != 0 && order_info['total_count'] != 0) {
+        now = new Date()
+        elapsed_sec = Math.round((now.getTime() - start_time.getTime()) / 1000)
+        remaining_sec = (elapsed_sec / rate) * (100 - rate)
+
+        var remaining_text
+        if (remaining_sec < 300) {
+            remaining_text = Math.round(remaining_sec) + '秒'
+        } else {
+            remaining_text = Math.round(remaining_sec / 60).toLocaleString() + '分'
+        }
+
+        document.getElementById('remaining_time').innerText = remaining_text
+    } else {
+        document.getElementById('remaining_time').innerText = '?'
+    }
 }
 
 function getNewFileHandle() {
@@ -84,8 +106,9 @@ function get_item_in_year(year, page, callback) {
         function (response) {
             for (item of response['list']) {
                 item_list.push(item)
+                order_info['total_price'] += item['price']
             }
-            order_info['done'] += response['order_count']
+            order_info['done_count'] += response['order_count']
             notify_progress()
 
             if (response['is_last']) {
@@ -105,7 +128,7 @@ function get_order_count_in_year(year, callback) {
             year: year
         },
         function (response) {
-            order_info['total'] += response['count']
+            order_info['total_count'] += response['count']
             notify_progress()
             callback()
         }
@@ -154,7 +177,7 @@ async function get_year_list() {
         .then(() => {
             log_append('完了しました．')
 
-            order_info['total'] = order_info['done']
+            order_info['total_count'] = order_info['done_count']
             notify_progress()
 
             document.getElementById('start').disabled = false
