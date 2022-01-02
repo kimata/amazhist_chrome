6be10742e44e303a6d8dad0cbbaa9bc476d480cc
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-01-02 18:12:08 +0900

    feat: JSON ファイルの保存まで対応

--- src/ctrl/ctrl.js
+++ src/ctrl/ctrl.js
@@ -1,16 +1,61 @@
-document.getElementById('start').onclick = function () {
-    chrome.runtime.sendMessage({ type: 'port' }, function () {
-        chrome.runtime.sendMessage(
-            {
-                type: 'parse',
-                target: 'list',
-                year: 2018,
-                page: 1
-            },
-            function (response) {
-                console.log(response)
+var item_list = []
+
+function get_item_in_year(year, page, callback) {
+    chrome.runtime.sendMessage(
+        {
+            type: 'parse',
+            target: 'list',
+            year: year,
+            page: page
+        },
+        function (response) {
+            for (item of response['list']) {
+                item_list.push(item)
             }
-        )
+
+            if (response['is_last']) {
+                callback()
+            } else {
+                get_item_in_year(year, page + 1, callback)
+            }
+        }
+    )
+}
+
+function getNewFileHandle() {
+    const options = {
+        types: [
+            {
+                description: 'JSON Files',
+                accept: {
+                    'text/json': ['.json']
+                }
+            }
+        ]
+    }
+    return window.showSaveFilePicker(options)
+}
+
+async function write(data) {
+    const handle = await getNewFileHandle()
+
+    const writable = await handle.createWritable()
+    await writable.write(JSON.stringify(data))
+    await writable.close()
+}
+
+document.getElementById('save').onclick = function () {
+    write(item_list)
+}
+
+document.getElementById('start').onclick = function () {
+    document.getElementById('start').disabled = true
+
+    item_list = []
+    chrome.runtime.sendMessage({ type: 'port' }, function () {
+        get_item_in_year(2001, 1, function () {
+            console.log(JSON.stringify(item_list))
+        })
     })
 }
 
